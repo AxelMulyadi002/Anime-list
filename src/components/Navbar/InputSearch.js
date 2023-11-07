b0f07087dc8839e087ca86d03f7a20e2c6af5c24
Axel Mulyadi <azelmulyadi02@gmail.com>
2023-11-07 09:39:30 +0700

    update feature enter key and remove input after enter

--- src/components/Navbar/InputSearch.js
+++ src/components/Navbar/InputSearch.js
@@ -3,34 +3,31 @@
 import { MagnifyingGlass } from '@phosphor-icons/react';
 import { useRouter } from 'next/navigation';
 import { useRef } from 'react';
-import { useEffect } from 'react';
 
 const InputSearch = () => {
 	const searchRef = useRef();
 	const router = useRouter();
 
-	useEffect(() => {
-		document.addEventListener('keydown', detectKeyDown, true);
-	}, []);
-
 	const handleSearch = (event) => {
 		event.preventDefault();
 		const keyword = searchRef.current.value;
 		router.push(`/search/${keyword}`);
-		console.log(e.key);
+		searchRef.current.value = '';
 	};
 
 	return (
 		<div className="relative">
-			<input
-				placeholder="Cari Anime"
-				className="p-2 rounded-md w-full"
-				ref={searchRef}
-			/>
+			<form onSubmit={handleSearch}>
+				<input
+					placeholder="Cari Anime"
+					className="p-2 rounded-md w-full"
+					ref={searchRef}
+				/>
 
-			<button className="absolute top-2 end-2" onClick={handleSearch}>
-				<MagnifyingGlass size={24} />
-			</button>
+				<button className="absolute top-2 end-2" type="submit">
+					<MagnifyingGlass size={24} />
+				</button>
+			</form>
 		</div>
 	);
 };
